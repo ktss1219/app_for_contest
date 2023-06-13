772fc17533730878c4af7d26e4762315b1f532dd
Shusaku Kita <115199227+ktss1219@users.noreply.github.com>
2023-06-13 15:46:44 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+@app.message("こんにちは")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"こんにちは！ <@{message['user']}> さん！")
+
+# アプリ起動
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
