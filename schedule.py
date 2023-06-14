582fb0c2a04f22abd3736adaf1327a152a28ca3f
ktss1219 <shusakukita@gmail.com>
2023-06-14 11:30:43 +0900

    更新

--- schedule.py
+++ schedule.py
@@ -1,6 +1,9 @@
 import os
 import json
+import firebase_admin
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
