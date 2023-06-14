a191c078c51f22463c8e125c2def8a64cfc0d21d
ktss1219 <shusakukita@gmail.com>
2023-06-14 17:15:57 +0900

    更新

--- schedule.py
+++ schedule.py
@@ -7,15 +7,16 @@ from datetime import datetime, timedelta
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-# UnixTime変換
+# 予定時刻（10分前）のUnixTime変換
 def convert_to_timestamp(year, month, day, hour, minute):
-    # 入力形式の文字列を日時オブジェクトに変換
+    # 入力を日時オブジェクトに変換
     datetime_obj = datetime(year, month, day, hour, minute)
 
     timestamp = (datetime_obj - datetime(1970, 1, 1, 9, 0)).total_seconds() - 600 # 10分前
 
     return int(timestamp)  # 整数値で返す
 
+# メッセージのスケジューリング
 def schedule_message(text, channel_id, scheduled_time):
     with open("JSON/scheduled_message.json", "r") as f:
         message_payload = json.load(f)
@@ -27,6 +28,7 @@ def schedule_message(text, channel_id, scheduled_time):
         blocks = message_payload["blocks"]
     )
 
+# ここは後々@app.actionに変更
 @app.message("test")
 def send_scheduled_message():
     # チャンネルID
@@ -36,7 +38,7 @@ def send_scheduled_message():
     text = "おはようございます！起床予定時刻の１０分前になりました！"
 
     # 予定時刻の計算（10分前）
-    scheduled_time = convert_to_timestamp(2023, 6, 14, 16, 34)# YYYYMMDDHHMM
+    scheduled_time = convert_to_timestamp(2023, 6, 14, 17, 24)# YYYYMMDDHHMM
 
     schedule_message(text, channel_id, scheduled_time)
 
